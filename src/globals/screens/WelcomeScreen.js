e660813f8ba997d40c42466852aadf612db22e9f
Prashanth <prashanth.bandogi@gmail.com>
2022-11-24 23:44:49 +0530

    FoodApp

---
+++ src/globals/screens/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+const WelcomeScreen = () => {
+  return (
+    <div>
+      <view>
+        <Text>WelcomeScreen</Text>
+      </view>
+    </div>
+  );
+}
+
+export default WelcomeScreen;
